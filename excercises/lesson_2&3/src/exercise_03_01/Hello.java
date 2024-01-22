0f311ee801020bc854b7488aff591462fa6b6232
THAM-SANQA <ndabat020@gmail.com>
2024-01-22 16:50:03 +0200

    Lessons 1-3

---
+++ excercises/lesson_2&3/src/exercise_03_01/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise_03_01;
+
+/**
+ *
+ * @author user1
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello People");
+    }
+    
+}
---
+++ excercises/lesson_2&3/src/exercise_03_02/ShoppingCart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise_03_02;
+
+/**
+ *
+ * @author user1
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the shopping cart!!");
+    }
+}
