254657df38d8b960cac9ba8cda466a6b69c962bf
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 11:29:31 +0200

    exercise 6.2

--- excercises/lesson6/src/exercise06_02/ShoppingCart.java
+++ excercises/lesson6/src/exercise06_02/ShoppingCart.java
@@ -5,18 +5,19 @@
 package exercise06_02;
 
 /**
- *
+ * 
  * @author thamsanqa
  */
 public class ShoppingCart {
+
     public static void main(String[] args) {
-        Item object_1 = new Item() , object_2 = new Item();
-        object_1.desc="Vest";
-        object_2.desc="Pants";
-        System.out.println("Object 1: "+object_1.desc);
-        System.out.println("Object 2: "+object_2.desc);
-        object_2=object_1;
-        System.out.println("Object 1: "+object_1.desc);
-        System.out.println("Object 2: "+object_2.desc);
+        Item object_1 = new Item(), object_2 = new Item();
+        object_1.desc = "Vest";
+        object_2.desc = "Pants";
+        System.out.println("Object 1: " + object_1.desc);
+        System.out.println("Object 2: " + object_2.desc);
+        object_2 = object_1;
+        System.out.println("Object 1: " + object_1.desc);
+        System.out.println("Object 2: " + object_2.desc);
     }
 }
