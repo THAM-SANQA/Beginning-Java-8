c1f7af1bbe6dbb62762023e0169df63d60b834de
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 10:30:20 +0200

    exercise 06 01

---
+++ excercises/lesson6/src/exercise06_01/Item.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise06_01;
+
+/**
+ *
+ * @author thamsanqa
+ */
+public class Item {
+
+    public static void main(String[] args) {
+        String custName = "Kat Niggress",
+            clothes[] = {"Vests", "Pants", "Perfumes", "Hats"},
+            message = custName + " is looking for " + clothes.length + " items.";
+        double price = 36.69, vat = 1.15;
+        int quantity = 1;
+        
+    }
+    
+}
