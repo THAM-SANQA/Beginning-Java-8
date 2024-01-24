3954cc63c8c44b216a11f76bd61608cc6302fadb
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 13:57:28 +0200

    practise 6.1

---
+++ practices/lesson6/06-ObjectsClasses_Practice1/src/soccer/League.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer;
+
+/**
+ *
+ * @author Thamsanqa Ndaba
+ */
+public class League {
+    public static void main(String[] args) {
+        Player player1 = new Player(), player2 = new Player(), player3 = new Player();
+        player1.plyerName = "Ramos"; 
+        player2.plyerName = "Oscar";
+        player3.plyerName = "Pele";
+        Player[] thePlayers = {player1, player2, player3};
+        Team team1 = new Team();
+        team1.teamName = "The Reds";
+        team1.playerArray = thePlayers;
+                
+        Team team2 = new Team();
+        team2.teamName = "The Zulus";
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].plyerName = "Zidane";
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].plyerName = "Carlos";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].plyerName = "Messi";
+        
+        for (Player thePlayer : team1.playerArray) {
+            System.out.println(thePlayer.plyerName);
+        }
+        
+        for (Player thePlayer : team2.playerArray) {
+            System.out.println(thePlayer.plyerName);
+        }
+    }
+}
---
+++ practices/lesson6/06-ObjectsClasses_Practice1/src/soccer/Team.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer;
+
+/**
+ *
+ * @author Thamsanqa Ndaba
+ */
+public class Team {
+    public String teamName;
+    public Player[] playerArray;
+}
