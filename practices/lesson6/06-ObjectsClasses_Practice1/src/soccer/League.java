c214af73be236ec636cd2192d8e17bfcd649a5d4
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 14:27:44 +0200

    practice 6.1

--- practices/lesson6/06-ObjectsClasses_Practice1/src/soccer/League.java
+++ practices/lesson6/06-ObjectsClasses_Practice1/src/soccer/League.java
@@ -9,30 +9,31 @@ package soccer;
  * @author Thamsanqa Ndaba
  */
 public class League {
+
     public static void main(String[] args) {
         Player player1 = new Player(), player2 = new Player(), player3 = new Player();
-        player1.plyerName = "Ramos"; 
-        player2.plyerName = "Oscar";
-        player3.plyerName = "Pele";
+        player1.plyerName = "Ramos";
+        player2.plyerName = "Ronaldo";
+        player3.plyerName = "Rafael";
         Player[] thePlayers = {player1, player2, player3};
         Team team1 = new Team();
-        team1.teamName = "The Reds";
+        team1.teamName = "The Rebels";
         team1.playerArray = thePlayers;
-                
+
         Team team2 = new Team();
         team2.teamName = "The Zulus";
         team2.playerArray = new Player[3];
         team2.playerArray[0] = new Player();
         team2.playerArray[0].plyerName = "Zidane";
         team2.playerArray[1] = new Player();
-        team2.playerArray[1].plyerName = "Carlos";
+        team2.playerArray[1].plyerName = "Zimmermann";
         team2.playerArray[2] = new Player();
-        team2.playerArray[2].plyerName = "Messi";
-        
+        team2.playerArray[2].plyerName = "Zlatan";
+
         for (Player thePlayer : team1.playerArray) {
             System.out.println(thePlayer.plyerName);
         }
-        
+
         for (Player thePlayer : team2.playerArray) {
             System.out.println(thePlayer.plyerName);
         }
