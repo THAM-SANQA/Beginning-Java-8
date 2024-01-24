568f86e5431a6719c6e53090fd2d846583ff29d0
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 11:04:52 +0200

    exercise 6 01

--- excercises/lesson6/src/exercise06_01/Item.java
+++ excercises/lesson6/src/exercise06_01/Item.java
@@ -10,13 +10,10 @@ package exercise06_01;
  */
 public class Item {
 
-    public static void main(String[] args) {
-        String custName = "Kat Niggress",
-            clothes[] = {"Vests", "Pants", "Perfumes", "Hats"},
-            message = custName + " is looking for " + clothes.length + " items.";
-        double price = 36.69, vat = 1.15;
-        int quantity = 1;
+    public int itemID;
+    public String desc;
+    public double price;
+    public int quantity;
         
-    }
     
 }
