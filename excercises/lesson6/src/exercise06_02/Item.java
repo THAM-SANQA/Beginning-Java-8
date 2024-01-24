a68d7989485b8a442e5d9290b569ccea655125d6
THAM-SANQA <ndabat020@gmail.com>
2024-01-24 11:27:22 +0200

    exercise 6.2

---
+++ excercises/lesson6/src/exercise06_02/Item.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise06_02;
+
+/**
+ *
+ * @author user1
+ */
+public class Item {
+    public int itemID;
+    public String desc;
+    public double price;
+    public int quantity;    
+}
---
+++ excercises/lesson6/src/exercise06_02/ShoppingCart.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise06_02;
+
+/**
+ *
+ * @author thamsanqa
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        Item object_1 = new Item() , object_2 = new Item();
+        object_1.desc="Vest";
+        object_2.desc="Pants";
+        System.out.println("Object 1: "+object_1.desc);
+        System.out.println("Object 2: "+object_2.desc);
+        object_2=object_1;
+        System.out.println("Object 1: "+object_1.desc);
+        System.out.println("Object 2: "+object_2.desc);
+    }
+}
