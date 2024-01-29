de11dcfa4a7ac8c509d5bb90b731c3b666a879cd
THAM-SANQA <ndabat020@gmail.com>
2024-01-29 17:27:07 +0200

    clswk 10

--- excercises/lesson10/src/classwork/Lesson10.java
+++ excercises/lesson10/src/classwork/Lesson10.java
@@ -20,29 +20,57 @@ public class Lesson10 {
 //        System.out.println((i < 1) || (j > 6));
 //        System.out.println(i < 3);
 //        System.out.println(!(i < 3));
-        int numberOfGoals = 1;
-        String s = (numberOfGoals == 1 ? "goal" : "goals");
-        System.out.println("I scored " + numberOfGoals + " " + s);
-        numberOfGoals++;
-        s = (numberOfGoals == 1 ? "goal" : "goals");
-        System.out.println("I scored " + numberOfGoals + " " + s);
+//        int numberOfGoals = 1;
+//        String s = (numberOfGoals == 1 ? "goal" : "goals");
+//        System.out.println("I scored " + numberOfGoals + " " + s);
+//        numberOfGoals++;
+//        s = (numberOfGoals == 1 ? "goal" : "goals");
+//        System.out.println("I scored " + numberOfGoals + " " + s);
 
-//        int month = 8;
-//        boolean isLeapYear = false;
-//        if (month > 12 || month < 1) {
-//            System.out.println("Invalid month.");
-//        } else if (month == 2) {
-//            if (!isLeapYear) {
-//                System.out.println("28 days in the month.");
-//            } else {
-//                System.out.println("29 days in the month.");
-//            }
-//        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-//            System.out.println("30 days in the month.");
-//        } else {
-//            System.out.println("31 days in the month.");
-//
-//        }
+        int month = 8;
+        boolean isLeapYear = false;
+        if (month > 12 || month < 1) {
+            System.out.println("Invalid month.");
+        } else if (month == 2) {
+            if (!isLeapYear) {
+                System.out.println("28 days in the month.");
+            } else {
+                System.out.println("29 days in the month.");
+            }
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            System.out.println("30 days in the month.");
+        } else {
+            System.out.println("31 days in the month.");
+
+        }
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 days in the month.");
+                break;
+            case 2:
+                if (!isLeapYear) {
+                    System.out.println("28 days in the month."
+                    );
+                } else {
+                    System.out.println("29 days in the month.");
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 days in the month.");
+                break;
+            default:
+                System.out.println("Invalid month.");
+        }
     }
 
 }
