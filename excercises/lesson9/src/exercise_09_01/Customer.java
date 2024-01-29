dbb8078653a37536f22186b301e1e66609d2e646
THAM-SANQA <ndabat020@gmail.com>
2024-01-29 12:26:23 +0200

    exercise 01

---
+++ excercises/lesson9/src/exercise_09_01/Customer.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise_09_01;
+
+/**
+ *
+ * @author Thamsanqa Ndaba
+ */
+public class Customer {
+    private String name;
+    private String ssn;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the ssn
+     */
+    public String getSsn() {
+        return ssn;
+    }
+
+    /**
+     * @param ssn the ssn to set
+     */
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
