25050ab340906f6b1c45ae33ac3e88590eeb90ec
THAM-SANQA <ndabat020@gmail.com>
2024-01-29 13:36:26 +0200

    ex 10.1

---
+++ excercises/lesson10/src/exercise_10_01/TestClass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise_10_01;
+
+/**
+ *
+ * @author Thamsanqa Ndaba
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        int x = 4, y = 9;
+        if (y / x < 3) {
+            x += y;
+        } else {
+            x *= y;
+        }
+        System.out.println("Original x = " + x);
+        x = 4;
+        y = 9;
+        x = ((y / x) < 3) ? (x += y) : (x *= y);
+        System.out.println("Replicate x = " + x);
+
+    }
+}
